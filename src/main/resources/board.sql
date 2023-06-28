9cd8639b1eb73f7c6f19df96a551a89e2d30be90
“suhyun” <suhyuuuuuun@gmail.com>
2023-06-28 21:40:46 +0900

    20230628 | sql문 수정

--- src/main/resources/board.sql
+++ src/main/resources/board.sql
@@ -1,3 +1,7 @@
+-- 테이블 생성 전 drop
+drop table second_board;
+
+-- 테이블 생성
 create table second_board (
 idx INT not null auto_increment,
 `group` varchar(100),
@@ -8,8 +12,10 @@ update_date datetime,
 primary KEY(idx)
 );
 
+-- 테이블 확인
 select * from trial.second_board;
 
+-- dummy data
 INSERT INTO SECOND_BOARD(`group`, title, contents, create_date) VALUES ('free', 'test_1', '내용 테스트1', now());
 INSERT INTO SECOND_BOARD(`group`, title, contents, create_date) VALUES ('free', 'test_2', '내용 테스트2', now());
 INSERT INTO SECOND_BOARD(`group`, title, contents, create_date) VALUES ('free', 'test_3', '내용 테스트3', now());
